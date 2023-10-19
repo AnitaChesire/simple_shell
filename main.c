4293de2bc927185da9184cc0d5ed7a6a142f1a46
AnitaChesire <bikamboanita@gmail.com>
2023-10-18 23:57:01 -0700

    new

--- main.c
+++ main.c
@@ -22,53 +22,52 @@ void sig_handler(int sig)
  */
 int main(int argc, char *argv[])
 {
-    int ret = 0, retn;
-    int *exe_ret = &retn;
-    char *prompt = "#cisfun$ ", *new_line = "\n";
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "#cisfun$ ", *new_line = "\n";
 
-    name = argv[0];
-    hist = 1;
-    aliases = NULL;
-    signal(SIGINT, sig_handler);
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
 
-    *exe_ret = 0;
-    environ = _copyenv();
-    if (!environ)
-        exit(-100);
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
 
-    if (argc != 1)
-    {
-        ret = proc_file_commands(argv[1], exe_ret);
-        free_env();
-        free_alias_list(aliases);
-        return *exe_ret;
-    }
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
 
-    if (!isatty(STDIN_FILENO))
-    {
-        while (ret != END_OF_FILE && ret != EXIT)
-            ret = handle_args(exe_ret);
-        free_env();
-        free_alias_list(aliases);
-        return *exe_ret;
-    }
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
 
-    while (1)
-    {
-        write(STDOUT_FILENO, prompt, 10);
-        ret = handle_args(exe_ret);
-        if (ret == END_OF_FILE || ret == EXIT)
-        {
-            if (ret == END_OF_FILE)
-                write(STDOUT_FILENO, new_line, 10);
-            free_env();
-            free_alias_list(aliases);
-            exit(*exe_ret);
-        }
-    }
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 10);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 10);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
 
-    free_env();
-    free_alias_list(aliases);
-    return *exe_ret;
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
-
