4c31ea78fa3f47712db826ef6ccd7762135caaf2
AnitaChesire <bikamboanita@gmail.com>
2023-10-19 01:02:22 -0700

    one

--- main.c
+++ main.c
@@ -1,31 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #define MAX_COMMAND_LENGTH 100
 
 int main() {
     char input[MAX_COMMAND_LENGTH];
     char *args[2];
-    int status;
-    pid_t pid;
+char status_msg[100];
+int len, status;
+pid_t pid;
+const char *prompt;
     
     while (1) {
-               printf("simple_shell> ");
+          prompt = "simple_shell> ";
+        write(STDOUT_FILENO, prompt, strlen(prompt));
         
         
         if (fgets(input, sizeof(input), stdin) == NULL) {
-        
-            printf("\n");
+                    write(STDOUT_FILENO, "\n", 1);
             break;
         }
         
+        
         input[strcspn(input, "\n")] = 0;
 
-        pid = fork();
+        
+         pid = fork();
         
         if (pid < 0) {
             perror("Fork failed");
@@ -33,20 +37,21 @@ int main() {
         }
         
         if (pid == 0) {
-                   args[0] = input;
+
+            args[0] = input;
             args[1] = NULL;
             
-                    if (execvp(input, args) == -1) {
+       
+            if (execvp(input, args) == -1) {
                 perror("Execution failed");
                 exit(1);
             }
         } else {
-      
-
-            waitpid(pid, &status, 0);
+                  waitpid(pid, &status, 0);
             
             if (WIFEXITED(status)) {
-                printf("Child exited with status %d\n", WEXITSTATUS(status));
+                len = snprintf(status_msg, sizeof(status_msg), "Child exited with status %d\n", WEXITSTATUS(status));
+                write(STDOUT_FILENO, status_msg, len);
             }
         }
     }
