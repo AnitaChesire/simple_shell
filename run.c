a52e5acccb6e299e45ba37a6a8031fee371d8235
AnitaChesire <bikamboanita@gmail.com>
2023-10-20 03:41:39 -0700

    env

--- run.c
+++ run.c
@@ -39,3 +39,41 @@ void run(char *cmd, char **argu, shellinfo_t *ourtype, char *buffer)
 		}
 	}
 }
+/**
+ * is_file - A function that checks if a particular name is a file or binary.
+ * @file_name: File to check if it has the permission to execute
+ * Return: 1 If is a file with the right permission, else -1.
+ */
+int is_file(char *file_name)
+{
+	int m, ego;
+
+	ego = _strlen(file_name);
+	for (m = 0; m < ego; m++)
+		if (file_name[m] == '/')
+		{
+			return (is_executable(file_name));
+		}
+
+	return (-1);
+}
+/**
+ * sub_env - A function used for replacing env variables.
+ * @ourtype: input as parameter.
+ * @envi_var: environment variable input.
+ * Return: pointer to the string.
+ */
+char *sub_env(shellinfo_t *ourtype, char *envi_var)
+{
+	char *envi;
+
+	(void) ourtype;
+
+	envi = get_env(envi_var);
+	if (envi != NULL)
+	{
+		return (envi);
+	}
+
+	return (NULL);
+}
